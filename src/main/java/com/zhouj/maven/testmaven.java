5e555e589a2f265024f5433dc446859154f86da0
zhoujinsy <949282784@qq.com>
2018-09-20 17:04:04 +0800

    first commit

---
+++ src/main/java/com/zhouj/maven/testmaven.java
@@ -0,0 +1,7 @@
+package com.zhouj.maven;
+
+public class testmaven {
+	public int add(int a,int b){
+		return a+b;
+	}
+}
---
+++ src/test/java/com/zhouj/maven/test/Test1.java
@@ -0,0 +1,14 @@
+package com.zhouj.maven.test;
+
+import org.junit.Test;
+
+import com.zhouj.maven.testmaven;
+
+public class Test1 {
+//	@Test
+//	public void testadd(){
+//		testmaven tm=new testmaven();
+//		int rs = tm.add(1,3);
+//		System.out.println(rs);
+//	}
+}
